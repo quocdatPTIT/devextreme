82fad90260eb7f0e347f8607c845fb209656c58f
dattq <dattq@metechvn.com>
2020-11-12 18:45:19 +0700

    add devextreme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
-
+import { Service } from './app.service';
 @Component({
-  selector: 'app-root',
+  selector: 'demo-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+  preserveWhitespaces: true
 })
 export class AppComponent {
-  title = 'prime';
+  lists: any[] = [];
+  statuses: string[] = [
+    'Not Started',
+    'Need Assistance',
+    'In Progress',
+    'Completed'
+  ];
+  employees: object = {};
+
+  constructor(service: Service) {
+    const tasks = service.getTasks();
+
+    service.getEmployees().forEach((employee) => {
+      this.employees[employee.ID] = employee.Name;
+    });
+
+    this.statuses.forEach((status) => {
+      this.lists.push(tasks.filter((task) => task.Task_Status === status));
+    });
+  }
+
+  onListReorder(e): void {
+    const list = this.lists.splice(e.fromIndex, 1)[0];
+    this.lists.splice(e.toIndex, 0, list);
+
+    const status = this.statuses.splice(e.fromIndex, 1)[0];
+    this.statuses.splice(e.toIndex, 0, status);
+  }
+
+  onTaskDragStart(e): void {
+    e.itemData = e.fromData[e.fromIndex];
+  }
+
+  onTaskDrop(e): void {
+    e.fromData.splice(e.fromIndex, 1);
+    e.toData.splice(e.toIndex, 0, e.itemData);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxScrollViewModule } from 'devextreme-angular/ui/scroll-view';
+import { DxSortableModule } from 'devextreme-angular/ui/sortable';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, DxScrollViewModule, DxSortableModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
